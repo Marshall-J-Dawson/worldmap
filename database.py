edd6ddab67408988c24ae99c3aacc767f8fc4d71
marshall-j-dawson <marshalljdawson@outlook.com>
2026-07-20 17:08:41 +1000

    wrote SQLite db construction

--- database.py
+++ database.py
@@ -1,1 +1,27 @@
 #generating the db
+import sqlite3
+
+#define connection to db
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS locations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    timestamp TEXT NOT NULL,
+    battery INTEGER
+)
+""")
+
+#save changes
+connection.commit()
+
+#close db
+connection.close()
+
+print("database created succesfully")
